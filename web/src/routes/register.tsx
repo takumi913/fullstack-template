d87249be3a3f6fdd35a7d68add7cd7d59bbe8bc1
阿杰鲁 <y.bz@foxmail.com>
2026-09-18 11:56:22 +0800

    feat(seo): add SSG and SPA route architecture

---
+++ web/src/routes/register.tsx
@@ -0,0 +1,9 @@
+import RegisterPage from "@/pages/RegisterPage";
+import { privatePageMeta } from "@/seo/page";
+
+export const meta = () => [
+  { title: "注册 | Fullstack Template" },
+  ...privatePageMeta,
+];
+
+export default RegisterPage;
