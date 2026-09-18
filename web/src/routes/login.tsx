cfaafb3409243a666a91807cf1278cedf01b25c4
阿杰鲁 <y.bz@foxmail.com>
2026-09-18 12:07:07 +0800

    style(web): apply Prettier formatting

--- web/src/routes/login.tsx
+++ web/src/routes/login.tsx
@@ -2,10 +2,7 @@ import LoginPage from "@/pages/LoginPage";
 import { PublicRoute } from "@/router/RouteGuards";
 import { privatePageMeta } from "@/seo/page";
 
-export const meta = () => [
-  { title: "登录 | Fullstack Template" },
-  ...privatePageMeta,
-];
+export const meta = () => [{ title: "登录 | Fullstack Template" }, ...privatePageMeta];
 
 export default function LoginRoute() {
   return (
