eeb4e62e5d2687d37dc6831daad9a7edc83965a5
阿杰鲁 <y.bz@foxmail.com>
2026-09-18 18:10:07 +0800

    perf(web): split public and authenticated layout dependencies

---
+++ web/src/components/layout/PublicLayout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import { Footer } from "./Footer";
+import { PublicHeader } from "./PublicHeader";
+
+export function PublicLayout() {
+  return (
+    <div className="flex min-h-screen flex-col bg-white">
+      <PublicHeader />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
