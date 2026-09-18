b474b91a828f2cccb2186b2ef15f77395ce468c6
阿杰鲁 <y.bz@foxmail.com>
2026-09-18 18:11:47 +0800

    refactor(web): export explicit route layouts

--- web/src/components/layout/index.ts
+++ web/src/components/layout/index.ts
@@ -1,1 +1,6 @@
-export * from "./Layout";
+export * from "./AppHeader";
+export * from "./AppLayout";
+export * from "./AuthLayout";
+export * from "./Footer";
+export * from "./PublicHeader";
+export * from "./PublicLayout";
