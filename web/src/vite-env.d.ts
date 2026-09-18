48d1f86049a0996ee16a81a0bb4319cbdbb5ea61
阿杰鲁 <y.bz@foxmail.com>
2026-09-18 18:03:34 +0800

    chore(web): type favicon build override

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -6,6 +6,7 @@ interface ImportMetaEnv {
   readonly VITE_SITE_NAME?: string;
   readonly VITE_SITE_SHORT_NAME?: string;
   readonly VITE_SITE_MARK?: string;
+  readonly VITE_SITE_FAVICON?: string;
   readonly VITE_SITE_LOCALE?: string;
   readonly VITE_SITE_PRIMARY_KEYWORD?: string;
   readonly VITE_SITE_TITLE?: string;
